2e13f00b2a00b11575e3c33aa82d78c5b1c6cd54
Alexander <icsd15029@icsd.aegean.gr>
2017-10-18 02:53:53 +0300

    Update ask1_b.s

--- 2017/ask1_b.s
+++ 2017/ask1_b.s
@@ -1,48 +1,43 @@
-.data
+#Γέροντας Αλέξανδρος 321/2015029
+
+# Δήλωση μεταβλητών
+.data 
 	msg0: .asciiz "Dose ton proto arithmo: " # Δήλωση του str1
 	msg1: .asciiz "Dose ton deutero arithmo: " # Δήλωση του str2
 	msg2: .asciiz "Apotelesma: "
-	var0: .word 10
-	var1: .word 5
+
+# Το πρόγραμμα τοποθετείται κάτω απο το.text
 .text
 
-main:
-	# printf(msgt1)
+# Κυρίως πρόγραμμα 
+main: 
 	la $a0,msg0
 	li $v0,4 	
-	syscall
-
-	# Eiσαγωγη τιμης απο το πληκτρολογιο (στο v0)
-	li $v0,5 
-	syscall
+	syscall # Εκτέλεση
 	
-	# akiro sxolio s2 = v0 , v0 = 0
+	li $v0,5 
+	syscall # Εκτέλεση	
+	
 	move $t0,$v0 
-	
-	# printf(msg2)
+		
 	la $a0,msg1
-	li $v0,4 	
-	syscall
+	li $v0,4 # Κλήση για εκτέλεση εκτύπωσης string	
+	syscall # Εκτέλεση
 
-	# Eiσαγωγη τιμης απο το πληκτρολογιο (στο v0)
-	li $v0,5 
-	syscall
+	li $v0, 5 # Κλήση για διάβασμα ακεραίου
+	syscall # Εκτέλεση	
+
+	move $t1,$v0 # Αποθήκευση της τιμής στον καταχωρητή t1	
+
+	sub $s0, $t0, $t1 #Εκτέλεση της πράξης αφαίρεσης 
 	
-	# akiro sxolio s2 = v0 , v0 = 0
-	move $t1,$v0 
-	
-	# s0 = s0 - s1
-	sub $s0, $t0, $t1
-	
-	# printf(msg2)
 	la $a0,msg2
 	li $v0,4 	
-	syscall
+	syscall # Εκτέλεση
 	
-	# printf(s0)
 	li $v0,1 	
-	move $a0,$s0
-	syscall
+	move $a0,$s0 # Αποθήκευση της τιμής του s0 στον καταχωρητή a0
+	syscall # Εκτέλεση
 	
-	li $v0,10 # exit system call
-	syscall # This ends execution
+	li $v0,10 # Κλήση για έξοδο
+	syscall # Εκτέλεση
