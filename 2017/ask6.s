db0c15b548473f3ab3cfd017b0d88853a2ec9e59
Alexander <icsd15029@icsd.aegean.gr>
2017-12-24 00:00:19 +0200

    Update ask6.s

--- 2017/ask6.s
+++ 2017/ask6.s
@@ -5,22 +5,22 @@
 .data	
 	array: .word 0:10 # Δεσμεύει έναν πίνακα 10 ακεραίων, όπου όλοι είναι αρχικοποιημένοι στο 0
 	
-	space: .asciiz " "  
-	enter: .asciiz "\n" ######################
+	# Δήλωση μηνυμάτων που θα τυπωθούν στην κονσόλα		
+	space: .asciiz " "  	
+	msg0:  .asciiz "Give temperature "
+	msg1:  .asciiz ": "
+	msg2:  .asciiz "Enter number of years to calculate average temperature: "
+	msg3:  .asciiz "K has to be between 0 and 10 \n"
+	msg4:  .asciiz "Average temperatures per "
+	msg5:  .asciiz " years: \n"
 	
-	msg0: .asciiz "Give temperature "
-	msg1: .asciiz ": "
-	msg2: .asciiz "Enter number of years to calculate average temperature: "
-	msg3: .asciiz "K has to be between 0 and 10 \n"
-	msg4: .asciiz "Average temperatures per "
-	msg5: .asciiz " years: \n"
-	
-	k: .word 0
-	ArrayPos: .word 0
-	EndPos: .word 0 
+	k:         .word 0 # Μεταβλητή για την αποθήκευση του πλήθους των θερμοκρασιών
+	ArrayPos:  .word 0 # Μεταβλητή για τον τερματισμό της συνάρτησης
+	EndPos:    .word 0 # Μεταβλητή για τον τερματισμό της συνάρτησης PrintMovingSum
 	LoopCount: .word 1
 	
 .text
+	# Κυρίως πρόγραμμα
 	Main:			
 		li $t0, 0            # $t0 = 0		
 				
@@ -45,53 +45,53 @@
 		syscall              # Εκτέλεση		
 													 
 		PrintMovingSum:
-			li $t2, 0         # $t2 = 0
-			lw $a0, ArrayPos  # $a0 = ArrayPos
-			jal CalculateMovingSum 	  # Άλμα στην συνάρτηση CalculateMovingSum
+			li $t2, 0                   # $t2 = 0
+			lw $a0, ArrayPos            # $a0 = ArrayPos
+			jal CalculateMovingSum 	    # Άλμα στην συνάρτηση CalculateMovingSum
 						
-			mov.s $f12, $f0	  # $f12 = $f0
-			li $v0, 2         # Κλήση για εκτέλεση εκτύπωσης float           
-			syscall           # Εκτέλεση
+			mov.s $f12, $f0	            # $f12 = $f0
+			li $v0, 2                   # Κλήση για εκτέλεση εκτύπωσης float           
+			syscall                     # Εκτέλεση
 		
-			la $a0, space     # Αποθήκευση διεύθυνσης space στον καταχωρητή $a0
-			li $v0, 4         # Κλήση για εκτέλεση εκτύπωσης string	
-			syscall           # Εκτέλεση
+			la $a0, space               # Αποθήκευση διεύθυνσης space στον καταχωρητή $a0
+			li $v0, 4                   # Κλήση για εκτέλεση εκτύπωσης string	
+			syscall                     # Εκτέλεση
 												
-			lw $s0, EndPos    # $s0 = EndPos		
+			lw $s0, EndPos              # $s0 = EndPos		
 			
-			blt $s0, 40, PrintMovingSum # if $s0 < 40 goto PrintMovingSum			
+			bne $s0, 40, PrintMovingSum # if $s0 < 40 goto PrintMovingSum			
 		
-		li $v0, 10   # Κλήση για έξοδο
-		syscall      # Εκτέλεση
+		li $v0, 10           # Κλήση για έξοδο
+		syscall              # Εκτέλεση
 	
 	# Υπολογισμός κινούμενων μέσων θερμοκρασιών
 	CalculateMovingSum:
-		lw $t1, array($a0)     # Αποθήκευση της διεύθυνσης του στοιχειού στην θέση $a0 του πίνακα στον καταχωρητή $t1
+		lw $t1, array($a0)               # Αποθήκευση της διεύθυνσης του στοιχειού στην θέση $a0 του πίνακα στον καταχωρητή $t1
 		
-		add $t2, $t2, $t1      # $t2 = $t2 + $t1
-		add $a0, $a0, 4        # $a0 = $a0 + 4
+		add $t2, $t2, $t1                # $t2 = $t2 + $t1
+		add $a0, $a0, 4                  # $a0 = $a0 + 4
 						
-		mul $t4, $a1, 4	       # $t4 = $a1 * 4	
-		lw $t5, ArrayPos       # $t5 = ArrayPos
-		add $t4, $t4, $t5      # $t4 = $t4 + $t5
+		mul $t4, $a1, 4	                 # $t4 = $a1 * 4	
+		lw $t5, ArrayPos                 # $t5 = ArrayPos
+		add $t4, $t4, $t5                # $t4 = $t4 + $t5
 			
 		blt $a0, $t4, CalculateMovingSum # if t0 == 4*k break
 		
-		sw $a0, EndPos         # EndPos = $a0
+		sw $a0, EndPos                   # EndPos = $a0
 		
-		lw $t0, ArrayPos       # $t0 = ArrayPos		
-		add $t0, $t0, 4        # $t0 = $t0 + 4	
-		sw $t0, ArrayPos       # ArrayPos = $t0
+		lw $t0, ArrayPos                 # $t0 = ArrayPos		
+		add $t0, $t0, 4                  # $t0 = $t0 + 4	
+		sw $t0, ArrayPos                 # ArrayPos = $t0
 				
-		mtc1 $t2, $f0          # Μεταφορά του ακέραιου καταχωρητή $t2, στον καταχωρητή κινητής υποδιαστολής $f0
-		cvt.s.w $f0, $f0       # Μετατροπή της τιμής του καταχωρητή $f0 από ακέραιο σε κινητής υποδιαστολής
+		mtc1 $t2, $f0                    # Μεταφορά του ακέραιου καταχωρητή $t2, στον καταχωρητή κινητής υποδιαστολής $f0
+		cvt.s.w $f0, $f0                 # Μετατροπή της τιμής του καταχωρητή $f0 από ακέραιο σε κινητής υποδιαστολής
 		
-		mtc1 $a1, $f1          # Μεταφορά του ακέραιου καταχωρητή $a1, στον καταχωρητή κινητής υποδιαστολής $f1
-		cvt.s.w $f1, $f1       # Μετατροπή της τιμής του καταχωρητή $f1 από ακέραιο σε κινητής υποδιαστολής
+		mtc1 $a1, $f1                    # Μεταφορά του ακέραιου καταχωρητή $a1, στον καταχωρητή κινητής υποδιαστολής $f1
+		cvt.s.w $f1, $f1                 # Μετατροπή της τιμής του καταχωρητή $f1 από ακέραιο σε κινητής υποδιαστολής
 		
-		div.s $f0, $f0, $f1    # $f0 = $f0 / $f1			
+		div.s $f0, $f0, $f1              # $f0 = $f0 / $f1			
 			
-		jr $ra                 # Έξοδος από την συνάρτηση και επιστροφή στον καταχωρητή $ra
+		jr $ra                           # Έξοδος από την συνάρτηση και επιστροφή στον καταχωρητή $ra
 		
 	# Είσοδος θερμοκρασιών από τον χρήστη
 	GetTemperatures:
@@ -121,7 +121,8 @@
 		blt $t0, 40, GetTemperatures # if $t0 < 40 goto GetTemperatures
 				
 		jr $ra                       # Έξοδος από την συνάρτηση και επιστροφή στον καταχωρητή $ra
-
+		
+	# Είσοδος πλήθους των θερμοκρασιών από τον χρήστη
 	GetNumberOfYears:	
 		la $a0, msg2             # Αποθήκευση διεύθυνσης msg2 στον καταχωρητή $a0 
 		li $v0, 4                # Κλήση για εκτέλεση εκτύπωσης string	
