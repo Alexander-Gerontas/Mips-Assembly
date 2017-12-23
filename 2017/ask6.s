b71b9028ff2939146aa231f6b39708b7176fb42a
Alexander <icsd15029@icsd.aegean.gr>
2017-12-23 21:10:38 +0200

    Update ask6.s

--- 2017/ask6.s
+++ 2017/ask6.s
@@ -10,32 +10,31 @@
 	
 	msg0: .asciiz "Give temperature "
 	msg1: .asciiz ": "
-	msg2: .asciiz "Dose to k: "
+	msg2: .asciiz "Give k: "
 	msg3: .asciiz "K has to be between 0 and 10 \n"
 	msg4: .asciiz "Average temperatures per "
 	msg5: .asciiz " years: \n"
 	
 	k: .word 0
-	ArrayPos: .word 0 ##############
-	endpos: .word 0
+	ArrayPos: .word 0
+	EndPos: .word 0 
+	LoopCount: .word 1
 	
 .text
 	Main:			
 		li $t0, 0		
-		li $t1, 1
+				
+		jal GetTemperatures # Άλμα στην συνάρτηση GetTemperatures
+		#jal fill
 		
-		#jal GetNumbers
-		jal fill
-		
-		li $t0, 0 ######			
-		jal Print ######
+		li $t0, 0 ######				
 		
 		jal GetNumber	
 		lw $a1, k
 						
 		la $a0, msg4          
-		li $v0, 4               # Κλήση για εκτέλεση εκτύπωσης string	
-		syscall                 # Εκτέλεση
+		li $v0, 4  # Κλήση για εκτέλεση εκτύπωσης string	
+		syscall # Εκτέλεση
 		
 		lw $a0, k	
 		li $v0, 1          # Κλήση για εκτέλεση εκτύπωσης ακεραίου
@@ -58,7 +57,7 @@
 			li $v0, 4 # Κλήση για εκτέλεση εκτύπωσης string	
 			syscall # Εκτέλεση
 												
-			lw $s4, endpos			
+			lw $s4, EndPos			
 			
 			blt $s4, 40, func			
 		
@@ -77,63 +76,65 @@
 			
 		blt $a0, $t4, CalcAver # if t0 == 4*k break
 		
-		sw $a0, endpos
+		sw $a0, EndPos 
 		
 		lw $t0, ArrayPos		
 		add $t0, $t0, 4
 		sw $t0, ArrayPos
 				
-		mtc1 $t2, $f0
-		cvt.s.w $f0, $f0
+		mtc1 $t2, $f0    # Μεταφορά του ακέραιου καταχωρητή $t2, στον καταχωρητή κινητής υποδιαστολής $f0
+		cvt.s.w $f0, $f0 # Μετατροπή της τιμής του καταχωρητή $f0 από ακέραιο σε κινητής υποδιαστολής
 		
-		mtc1 $a1, $f1
-		cvt.s.w $f1, $f1
+		mtc1 $a1, $f1    # Μεταφορά του ακέραιου καταχωρητή $a1, στον καταχωρητή κινητής υποδιαστολής $f1
+		cvt.s.w $f1, $f1 # Μετατροπή της τιμής του καταχωρητή $f1 από ακέραιο σε κινητής υποδιαστολής
 		
-		div.s $f0, $f0, $f1			
+		div.s $f0, $f0, $f1 # $f0 = $f0 / $f1			
 			
-		jr $ra
+		jr $ra # Έξοδος από την συνάρτηση και επιστροφή στον καταχωρητή $ra
 		
 	# Είσοδος αριθμών από τον χρήστη
-	GetNumbers:
-		la $t2, array($t0)
+	GetTemperatures:
+		la $t1, array($t0) # Αποθήκευση της διεύθυνσης του πίνακα στη θέση $t0 στον καταχωρητή $t1
 		
-		la $a0, msg0
-		li $v0, 4               # Κλήση για εκτέλεση εκτύπωσης string	
-		syscall                 # Εκτέλεση
+		la $a0, msg0      # Αποθήκευση διεύθυνσης msg0 στον καταχωρητή $a0 
+		li $v0, 4         # Κλήση για εκτέλεση εκτύπωσης string	
+		syscall           # Εκτέλεση
 		
-		move $a0, $t1           
-		li $v0, 1               # Κλήση για εκτέλεση εκτύπωσης ακεραίου
-		syscall                 # Εκτέλεση
+		lw $a0, LoopCount # $a0 = LoopCount         
+		li $v0, 1         # Κλήση για εκτέλεση εκτύπωσης ακεραίου
+		syscall           # Εκτέλεση
 		
-		la $a0, msg1
-		li $v0, 4               # Κλήση για εκτέλεση εκτύπωσης string	
-		syscall                 # Εκτέλεση
+		add $a0, $a0, 1   # $a0 = $a0 + 1
+		sw $a0, LoopCount # LoopCount = $a0
 		
-		li $v0,5                # Κλήση για διάβασμα ακεραίου  
-		syscall                 # Εκτέλεση	
-		sw $v0, ($t2)          
+		la $a0, msg1      # Αποθήκευση διεύθυνσης msg1 στον καταχωρητή $a0
+		li $v0, 4         # Κλήση για εκτέλεση εκτύπωσης string	
+		syscall           # Εκτέλεση
+		
+		li $v0,5          # Κλήση για διάβασμα ακεραίου  
+		syscall           # Εκτέλεση	
+		sw $v0, ($t1)          
 			
-		add $t1, $t1, 1
 		add $t0, $t0, 4         
 	
-		blt $t0, 40, GetNumbers 
+		blt $t0, 40, GetTemperatures # if $t0 < 40 goto GetTemperatures
 				
 		jr $ra                  # Έξοδος από την συνάρτηση και επιστροφή στον καταχωρητή $ra
 
 	GetNumber:	
-		la $a0, msg2
-		li $v0, 4               # Κλήση για εκτέλεση εκτύπωσης string	
-		syscall                 # Εκτέλεση
+		la $a0, msg2             # Αποθήκευση διεύθυνσης msg2 στον καταχωρητή $a0 
+		li $v0, 4                # Κλήση για εκτέλεση εκτύπωσης string	
+		syscall                  # Εκτέλεση
 		
-		li $v0,5                # Κλήση για διάβασμα ακεραίου  
-		syscall                 # Εκτέλεση	
+		li $v0,5                 # Κλήση για διάβασμα ακεραίου  
+		syscall                  # Εκτέλεση	
 		
-		blez $v0, WrongMessage
-		bgt $v0, 9, WrongMessage
+		blez $v0, WrongMessage   # if $vo <= 0 goto WrongMessage
+		bgt $v0, 9, WrongMessage # if $vo > 9 goto WrongMessage
 		
-		sw $v0, k
+		sw $v0, k                # k = $v0
 			
-		jr $ra                  # Έξοδος από την συνάρτηση και επιστροφή στον καταχωρητή $ra
+		jr $ra                   # Έξοδος από την συνάρτηση και επιστροφή στον καταχωρητή $ra
 		
 	WrongMessage:
 		la $a0, msg3
@@ -142,26 +143,7 @@
 		
 		j GetNumber
 
-	##############
-	Print:	
-		lw $t2, array($t0) 
 	
-		move $a0, $t2      
-		li $v0, 1          # Κλήση για εκτέλεση εκτύπωσης ακεραίου
-		syscall            # Εκτέλεση
-
-		la $a0, space      # Αποθήκευση διεύθυνσης space στον καταχωρητή $a0
-		li $v0, 4          # Κλήση για εκτέλεση εκτύπωσης string
-		syscall            # Εκτέλεση
-	
-		add $t0, $t0, 4	   
-		blt $t0, 40, Print 
-		
-		la $a0, enter      # Αποθήκευση διεύθυνσης enter στον καταχωρητή $a0
-		li $v0, 4          # Κλήση για εκτέλεση εκτύπωσης string	
-		syscall            # Εκτέλεση
-	
-		jr $ra             # Έξοδος από την συνάρτηση και άλμα στον καταχωρητή $ra
 		
 
 	#################					
